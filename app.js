8d53d6f3dec0ed3dc8200491ef6484e4a5ffaebe
Hugo Alejandro Garduño Nava <gardunohugo@icloud.com>
2025-08-12 05:07:10 +0000

    se agrego la funcion agregarAmigo(); y la funcion agregarListaDeAmigos(listaDeAmigos);

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigosJs = [];
+
+
+function agregarAmigo(){
+    
+    let amigoNuevoJs = document.getElementById("amigo").value;
+    
+    if ( amigoNuevoJs == ""){
+            alert("Por favor inserte un nombre");
+        } else {
+            listaDeAmigosJs.push(amigoNuevoJs);
+             amigo.value = "";
+            console.log(listaDeAmigosJs);
+        } 
+    return agregaListaDeAmigos(listaDeAmigosJs);
+    
+}
+
+function agregaListaDeAmigos(listaDeAmigosJs){
+
+    const listaDeAmigosHtml = document.getElementById('listaAmigos');
+    
+    console.log(listaDeAmigosHtml);
+    listaDeAmigosHtml.innerHTML = '';
+
+    for (let i = 0; i < listaDeAmigosJs.length; i++){
+
+            let nuevoAmigoHtml = document.createElement('li');
+            listaDeAmigosHtml.innerHTML += `🔹${listaDeAmigosJs[i].toUpperCase()}<br>`;
+            
+    }
+}
